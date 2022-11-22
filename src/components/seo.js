9bc419609d736bfd08fcec1d1c0aed2b196b121c
Abdelhamid Mohamed <abdelhamid.mohamed@jumia.com>
2022-11-22 11:16:15 +0200

    update seo

--- src/components/seo.js
+++ src/components/seo.js
@@ -71,13 +71,15 @@ function SEO({ description, lang, meta, keywords, title }) {
         .concat(
           keywords.length > 0
             ? {
-                name: `keywords`,
-                content: keywords.join(`, `),
-              }
+              name: `keywords`,
+              content: keywords.join(`, `),
+            }
             : []
         )
         .concat(meta)}
-    />
+    >
+      <link rel="me" href="https://fosstodon.org/@hameed0z" />
+    </Helmet>
   )
 }
 
